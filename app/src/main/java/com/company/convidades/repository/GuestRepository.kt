a4d4a30ddd48c6f2b551b90fb5fc156ca8e4ea43
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-06-03 12:20:33 -0300

    fun: save, update and delete

--- app/src/main/java/com/company/convidades/repository/GuestRepository.kt
+++ app/src/main/java/com/company/convidades/repository/GuestRepository.kt
@@ -1,6 +1,10 @@
 package com.company.convidades.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.company.convidades.constants.DataBaseConstants
+import com.company.convidades.model.GuestModel
+import java.sql.SQLException
 
 class GuestRepository private constructor(context: Context) {
 
@@ -16,22 +20,66 @@ class GuestRepository private constructor(context: Context) {
             return repository
         }
 
-        fun save(){
+    }
 
+    fun save(guestModel: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val presence = if (guestModel.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guestModel.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, values)
+            true
+        } catch (ex: Exception) {
+            false
+        }
+    }
+
+    fun update(guestModel: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val presence = if (guestModel.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guestModel.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = "${DataBaseConstants.GUEST.COLUMNS.ID} = ?"
+
+            val args = arrayOf(guestModel.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+
+            true
+        } catch (ex: Exception) {
+            false
         }
 
-        fun update(){
 
+    }
+
+    fun findById(id: Int) {
+
+    }
+
+    fun delete(id: Int):Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selection = "${DataBaseConstants.GUEST.COLUMNS.ID} = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+
+            true
+        } catch (ex: Exception) {
+            false
         }
-
-        fun findById(id: Int){
-
-        }
-
-        fun delete(){
-
-        }
-
     }
 
 }
