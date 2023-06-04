f243be8d73c1d2e5a8497c3d74b3540704edffa1
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-06-04 20:23:13 -0300

    FindAll functions

--- app/src/main/java/com/company/convidades/repository/GuestRepository.kt
+++ app/src/main/java/com/company/convidades/repository/GuestRepository.kt
@@ -2,9 +2,9 @@ package com.company.convidades.repository
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import com.company.convidades.constants.DataBaseConstants
 import com.company.convidades.model.GuestModel
-import java.sql.SQLException
 
 class GuestRepository private constructor(context: Context) {
 
@@ -63,16 +63,12 @@ class GuestRepository private constructor(context: Context) {
 
     }
 
-    fun findById(id: Int) {
-
-    }
-
-    fun delete(id: Int):Boolean {
+    fun delete(id: Int): Boolean {
         return try {
-            val db = guestDataBase.writableDatabase
+            val db: SQLiteDatabase = guestDataBase.writableDatabase
 
             val selection = "${DataBaseConstants.GUEST.COLUMNS.ID} = ?"
-            val args = arrayOf(id.toString())
+            val args: Array<String> = arrayOf(id.toString())
 
             db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
 
@@ -80,6 +76,74 @@ class GuestRepository private constructor(context: Context) {
         } catch (ex: Exception) {
             false
         }
+    }
+
+    fun findAll():List<GuestModel> {
+
+        val guests = mutableListOf<GuestModel>()
+
+        try {
+
+            val db: SQLiteDatabase = guestDataBase.readableDatabase
+
+            val projection: Array<String> = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME,
+                projection, null, null,
+                null, null, null
+            )
+
+            if (cursor != null && cursor.count > 0){
+                while (cursor.moveToNext()){
+                    val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE))
+
+                    guests.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+            cursor.close()
+        } catch (ex: Exception){
+            return guests
+        }
+
+        return guests
+    }
+
+    fun findAllByPresence(presence:Int): List<GuestModel>{
+        val guests = mutableListOf<GuestModel>()
+
+        try {
+
+            val db: SQLiteDatabase = guestDataBase.readableDatabase
+
+            val cursor = db.rawQuery("SELECT * FROM Guest WHERE presence = $presence",null)
+
+            if (cursor != null && cursor.count > 0){
+                while (cursor.moveToNext()){
+                    val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE))
+
+                    guests.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+            cursor.close()
+        } catch (ex: Exception){
+            return guests
+        }
+        return guests
+    }
+
+    fun findById(id: Int) {
+
     }
 
 }
