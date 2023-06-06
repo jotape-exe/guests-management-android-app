89c060cc95b8c7a2a660250a756a562a8c5660be
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-06-06 16:30:38 -0300

    Click Events(Delete, onClick)

--- app/src/main/java/com/company/convidades/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/company/convidades/view/viewholder/GuestViewHolder.kt
@@ -6,11 +6,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.company.convidades.R
 import com.company.convidades.databinding.RowGuestBinding
 import com.company.convidades.model.GuestModel
+import com.company.convidades.view.listener.IGuestListener
 
-class GuestViewHolder(private val bind: RowGuestBinding) : RecyclerView.ViewHolder(bind.root) {
+class GuestViewHolder(private val bind: RowGuestBinding, private val listener: IGuestListener) :
+    RecyclerView.ViewHolder(bind.root) {
 
     fun bind(guestModel: GuestModel) {
         bind.textName.text = guestModel.name
+
+        bind.textName.setOnClickListener {
+            listener.onClick(guestModel.id)
+        }
+
+        bind.textName.setOnLongClickListener {
+            listener.onDelete(guestModel.id)
+            true
+        }
     }
 
 }
