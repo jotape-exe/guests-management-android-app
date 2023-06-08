2f12f3aed0b040f8aaa983dc5570c2c782265392
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-06-07 21:40:28 -0300

    find guest by id viewmodel layer

--- app/src/main/java/com/company/convidades/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/company/convidades/viewmodel/GuestFormViewModel.kt
@@ -2,14 +2,22 @@ package com.company.convidades.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.company.convidades.model.GuestModel
 import com.company.convidades.repository.GuestRepository
 
 class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
     private var repository = GuestRepository.getInstance(application)
+    private val guestModel = MutableLiveData<GuestModel>()
+    val guest: LiveData<GuestModel> = guestModel
     fun save(guestModel: GuestModel) {
         repository.save(guestModel)
+    }
+
+    fun get(id:Int) {
+
     }
 
 
