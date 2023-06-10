384d4ac81f600977c54e98828bc6e56fc6c1e785
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-06-10 16:53:22 -0300

    refactor model -> Room

--- app/src/main/java/com/company/convidades/model/GuestModel.kt
+++ app/src/main/java/com/company/convidades/model/GuestModel.kt
@@ -1,4 +1,19 @@
 package com.company.convidades.model
 
-data class GuestModel(val id: Int, var name: String, var presence: Boolean) {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Guest")
+class GuestModel {
+
+    @ColumnInfo
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = false
 }
